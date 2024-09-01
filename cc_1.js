e88d775dde484c44992adaabf55821c2cbd0510b
Aleyana McLeod <aleyanamcleod1@gmail.com>
2024-09-01 18:49:09 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,11 @@
+// Task 1 Variables and Data Types
+
+let employeeName = "Benjamin Carter";
+const employeeID = 2987;
+var isActive = true;
+
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive, typeof(isActive));
+
+
